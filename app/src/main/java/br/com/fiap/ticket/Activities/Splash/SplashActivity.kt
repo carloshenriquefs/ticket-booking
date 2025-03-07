3df59ad49302a1cf2ab7b835d34f8855eba49bec
Carlos Henrique <ichfs9108@gmail.com>
2025-03-07 15:53:40 -0300

    project created

---
+++ app/src/main/java/br/com/fiap/ticket/Activities/Splash/SplashActivity.kt
@@ -0,0 +1,20 @@
+package br.com.fiap.ticket.Activities.Splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
+import br.com.fiap.ticket.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
